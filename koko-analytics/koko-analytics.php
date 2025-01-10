f192d73f4609360c7e7a77c9df3f50b61b492885
Sean Murphy <sean@iamseanmurphy.com>
2025-01-10 19:16:36 +0000

    1.5.5

--- koko-analytics/koko-analytics.php
+++ koko-analytics/koko-analytics.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Koko Analytics
 Plugin URI: https://www.kokoanalytics.com/#utm_source=wp-plugin&utm_medium=koko-analytics&utm_campaign=plugins-page
-Version: 1.5.4
+Version: 1.5.5
 Description: Privacy-friendly analytics for your WordPress site.
 Author: ibericode
 Author URI: https://www.ibericode.com/
@@ -34,13 +34,18 @@ phpcs:disable PSR1.Files.SideEffects
 
 namespace KokoAnalytics;
 
-\define('KOKO_ANALYTICS_VERSION', '1.5.4');
+\define('KOKO_ANALYTICS_VERSION', '1.5.5');
 \define('KOKO_ANALYTICS_PLUGIN_FILE', __FILE__);
 \define('KOKO_ANALYTICS_PLUGIN_DIR', __DIR__);
 
 // Load the Koko Analytics autoloader
 require __DIR__ . '/autoload.php';
 
+// Maybe run any pending database migrations
+$migrations = new Migrations('koko_analytics_version', KOKO_ANALYTICS_VERSION, KOKO_ANALYTICS_PLUGIN_DIR . '/migrations/');
+$migrations->maybe_run();
+
+// Initialize rest of plugin
 if (\defined('DOING_AJAX') && DOING_AJAX) {
     add_action('init', '\KokoAnalytics\maybe_collect_request', 1, 0);
 } elseif (is_admin()) {
@@ -52,7 +57,6 @@ if (\defined('DOING_AJAX') && DOING_AJAX) {
 }
 
 new QueryLoopBlock();
-new Migrations('koko_analytics_version', KOKO_ANALYTICS_VERSION, KOKO_ANALYTICS_PLUGIN_DIR . '/migrations/');
 new Dashboard();
 $aggregator = new Aggregator();
 new Plugin($aggregator);
