a2eede36aa4807861a8bb7e7ae5afd8817fdec6f
Sean Murphy <sean@iamseanmurphy.com>
2025-08-28 18:18:36 +0000

    2.0.9

--- koko-analytics/src/class-aggregator.php
+++ koko-analytics/src/class-aggregator.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * @package koko-analytics
- * @license GPL-3.0+
- * @author Danny van Kooten
+ * This file is loaded from the 1.8.x autoloader and that's why we keep it around.
+ * This helps smoothens the upgrade to 2.x by at least preventing the error directly after the update (using the 1.8.x autoloader which is in memory at that point)
  */
 
 namespace KokoAnalytics;
